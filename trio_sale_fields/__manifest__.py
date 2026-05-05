2d9ff2acdc081c02a3b4d24ed4cca8dc94cd79da
igbibgs <igbibgs@github.com>
2026-05-05 21:16:16 +0000

    feat: add trio_sale_fields module - restore Studio custom fields on sale order form and list views

---
+++ trio_sale_fields/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Trio Sale Fields',
+    'version': '19.0.1.0.0',
+    'summary': 'Restores Studio custom fields visibility on Sale Order form and list views',
+    'author': 'Trio Home Group',
+    'license': 'LGPL-3',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
